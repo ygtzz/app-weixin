9c53f0f9b8a6fe960befcfad7820e9be9797be49
mengwei <mengwei@papayamobile.cn>
2017-02-08 18:13:35 +0800

    add contract friend list

--- src/pages/index/views/contract.vue
+++ src/pages/index/views/contract.vue
@@ -43,14 +43,54 @@
                 </div>
             </li>
         </ul>
+        <ul class="contact-friends">
+            <li v-for="grouping in contact_friends"> 
+                <p class="contact-alpha" v-text="grouping.alpha"></p>
+                <div class="weui_cells weui_cells_access">
+                    <div class="weui_cell"
+                    v-touch:tap="go_personInfo(item.id)"
+                    v-for="item in grouping.list">
+                        <div class="weui_cell_hd">
+                            <img :src="item.iconSrc"></div>
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p v-text="item.remark"></p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
 import searchBar from '../widget/searchbar.vue';
 import imgHolder from '../widget/imgHolder.vue';
+import {mapGetters,mapActions} from 'vuex';
 
 export default {
     name:'c-address',
+    created(){
+        this.get_friends_list()
+        this.set_menu_active(1)
+    },
+    computed:{
+        ...mapGetters({
+            contact_friends:'contact_friends'
+        })
+    },
+    methods:{
+        ...mapActions({
+            et_menu_active:'et_menu_active',
+            get_friends_list:'get_friends_list',
+            set_friends_id:'set_friends_id',
+            get_person_info:'get_person_info',
+            set_menu_active:'set_menu_active'
+        }),
+        go_personInfo(id){
+            this.get_person_info(id,()=>{
+                this.$router.go({'path':'/contact/person-info'})
+            })
+        }
+    },
     components:{
         'c-searchbar':searchBar,
         'c-imgHolder':imgHolder
